b4687ba38bffce87c55e94d49f1112e8dbbcc131
RuzikM <ruzanna.martirosyan@yahoo.com>
2013-07-08 22:18:17 +0400

    nayeq DBcontrol

--- Chat/DbControl.cs
+++ Chat/DbControl.cs
@@ -32,16 +32,19 @@ namespace WpfApplication1
         
         private void DBCreate()
         {
-            const string databaseName = @"data.db";
+            //const string databaseName = @"data.db";
             if (!File.Exists(databaseName))
             {
                 SQLiteConnection.CreateFile(databaseName);
-                connection =
-                new SQLiteConnection(string.Format("Data Source={0};", databaseName));
-                SQLiteCommand command =
-                new SQLiteCommand("CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT);", connection);
+                connection = new SQLiteConnection(string.Format("Data Source={0};", databaseName));
+
+                SQLiteCommand usersname =
+                    new SQLiteCommand("CREATE TABLE users (Id integer PRIMARY KEY NOT NULL AUTOINCREMENT , name  varchar(200) NOT NULL);", connection);
+                // erexeq 2-rd tablen ela stex creat linum?
+                // SQLiteCommand Userschat =
+                //new SQLiteCommand("CREATE TABLE message (Id integer PRIMARY KEY NOT NULL AUTOINCREMENT , messages  varchar(200) NOT NULL, U-Id INTEGER  NOT NULL);", connection);
                 connection.Open();
-                command.ExecuteNonQuery();
+                usersname.ExecuteNonQuery();
                 connection.Close();
 
             }
@@ -50,9 +53,10 @@ namespace WpfApplication1
 
         }
 
-        private void DBinsert(string username)
+        
+        private void DBinsertUsername(string username)
         {
-            SQLiteCommand command = new SQLiteCommand("INSERT INTO 'users' ('id', 'name') VALUES (1, '');", connection);
+            SQLiteCommand command = new SQLiteCommand("INSERT INTO 'users' ('id', 'name') VALUES (1, 'username');", connection);
             command.ExecuteNonQuery();
 
 
