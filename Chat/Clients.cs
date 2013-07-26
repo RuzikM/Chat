cc045f5a817caf5e2774e1819c77b0595be6eeff
Karen <karen9331@mail.ru>
2013-07-26 17:45:27 +0400

    cofmmit

--- Chat/Clients.cs
+++ Chat/Clients.cs
@@ -8,13 +8,31 @@ namespace WpfApplication1
 {
     class Clients
     {
+        string ip="";
+        public Clients()
+        {
+
+   IPHostEntry host;
+   string localIP = "";
+   host = Dns.GetHostEntry(Dns.GetHostName());
+   foreach (IPAddress ip in host.AddressList)
+   {
+     if (ip.AddressFamily == AddressFamily.InterNetwork)
+     {
+       localIP = ip.ToString();
+       break;
+     }
+   }
+   this.ip=localIP;
+
+        }
         public string name { get; set; }
 
         public void SendName(string name)
         {
             Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
             IPEndPoint iep1 = new IPEndPoint(IPAddress.Broadcast, 8101);
-            IPEndPoint iep2 = new IPEndPoint(IPAddress.Parse("192.168.1.255"), 8101);
+            IPEndPoint iep2 = new IPEndPoint(IPAddress.Parse(this.ip), 8101);
 
             string username =name ;
             byte[] data = Encoding.ASCII.GetBytes("*us*"+username);
@@ -29,7 +47,7 @@ namespace WpfApplication1
         {
             Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
             IPEndPoint iep1 = new IPEndPoint(IPAddress.Broadcast, 8101);
-            IPEndPoint iep2 = new IPEndPoint(IPAddress.Parse("192.168.1.255"), 8101);
+            IPEndPoint iep2 = new IPEndPoint(IPAddress.Parse(this.ip), 8101);
 
             string msg = message;
             byte[] data = Encoding.ASCII.GetBytes(msg);
